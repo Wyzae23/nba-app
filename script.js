926f00fe90d8d45dee118da43693e7c8fd13a4f2
Harold <haroldmo23@gmail.com>
2022-08-30 17:02:06 -0700

    checks for null values

--- script.js
+++ script.js
@@ -15,15 +15,73 @@ let main =
         let player = data.data[0];
         let temp = document.getElementById("playerInfoTemplate").content;
         let tempHTML = document.importNode(temp, true);
-        tempHTML.querySelector(".player-name").textContent = player.first_name + " " + player.last_name;
-        tempHTML.querySelector(".position").textContent = player.position;
-        tempHTML.querySelector(".height-feet").textContent = player.height_feet;
-        tempHTML.querySelector(".height-inches").textContent = player.height_inches;
-        tempHTML.querySelector(".weight").textContent = player.weight_pounds;
-        tempHTML.querySelector(".team").textContent = player.team.full_name;
+        if(player.first_name != null)
+        {
+            tempHTML.querySelector(".player-name").textContent = "Name: " + player.first_name;
+        }
+        if(player.last_name != null)
+        {
+            tempHTML.querySelector(".player-name").textContent += (" " + player.last_name);
+        }
+        if(player.position != null)
+        {
+            tempHTML.querySelector(".position").textContent = "Position: " + player.position;
+        }
+        if(player.height_feet != null)
+        {
+            tempHTML.querySelector(".height").textContent = "Height: " + player.height_feet + " feet";
+        }
+        if(player.height_inches != null)
+        {
+            tempHTML.querySelector(".height").textContent += " " + player.height_inches + " inches";
+        }
+        if(player.weight != null)
+        {
+            tempHTML.querySelector(".weight").textContent = "Weight: " + player.weight_pounds;        
+        }
+        if(player.team.full_name != null)
+        {
+            tempHTML.querySelector(".team").textContent = "Team: " + player.team.full_name;
+        }
         let playerInfoClass = document.getElementsByClassName("player-info")[0];
         playerInfoClass.classList.remove("invisible");
         playerInfoClass.innerHTML = "";
+        playerInfoClass.appendChild(tempHTML);
+        let playerId = player.id;
+        this.getPlayerStats(playerId);
+    },
+
+    getPlayerStats: function(playerId)
+    {
+        fetch("https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + playerId)
+        .then((response) => response.json())
+        .then((data) => this.displayPlayerStats(data));
+    },
+
+    displayPlayerStats: function(data)
+    {
+        player = data.data[0];
+        let ppg = player.pts;
+        let rpg = player.reb;
+        let apg = player.ast;
+        let spg = player.stl;
+        let bpg = player.blk;
+        let threesPerGame = player.fg3m;
+        let fgp = player.fp_pct;
+        let ftp = player.ft_pct;
+        let tpg = player.turnover;
+        let temp = document.getElementById("playerStatsTemplate").content;
+        let tempHTML = document.importNode(temp, true);
+        tempHTML.querySelector(".points-per-game").textContent = ppg;
+        tempHTML.querySelector(".rebounds-per-game").textContent = rpg;
+        tempHTML.querySelector(".assists-per-game").textContent = apg;
+        tempHTML.querySelector(".steals-per-game").textContent = spg;
+        tempHTML.querySelector(".blocks-per-game").textContent = bpg;
+        tempHTML.querySelector(".threes-per-game").textContent = threesPerGame;
+        tempHTML.querySelector(".field-goal-percentage").textContent = fgp;
+        tempHTML.querySelector(".free-throw-percentage").textContent = ftp;
+        tempHTML.querySelector(".turnovers-per-game").textContent = tpg;
+        let playerInfoClass = document.getElementsByClassName("player-info")[0];
         playerInfoClass.appendChild(tempHTML);
     },
 
